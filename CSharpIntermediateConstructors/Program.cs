6241379bc96df186c5098982bee33c88ae10abdf
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-22 10:02:00 -0600

    Add project files.

---
+++ CSharpIntermediateConstructors/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+/*
+ 
+What is a Constructor?
+
+ Constructor is a method that is called when an instance of a class is created.
+
+Why do we need a constructor.
+
+The intention is to put an object in an earlier state that is to initialize some of the fields in the
+
+class.
+
+
+public class Customer
+{
+    public Customer()
+    {
+    
+    }
+}
+ */
